6ca0b72aeec75fcf34f5601b1c6be26395d34616
Marpol96 <51094297+Marpol96@users.noreply.github.com>
2019-11-09 14:53:43 -0600

    Avance2

--- Project/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Project/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun ChekcApuesta(){
+        if(text_Apuesta.text.toString().isEmpty()){
+            Toast.makeText(this,"No se Puede Empesar Sin Una Apuesta",Toast.LENGTH_SHORT).show()
+        }else if(text_Apuesta.text.toString()>text_Credit.text.toString()){
+            Toast.makeText(this,"No se Puede Empesar Sin Tu Apuesta Es Mayor Al Credito",Toast.LENGTH_SHORT).show()
+        }else if(text_Apuesta.text.toString()<text_Credit.text.toString()){
+            Toast.makeText(this,"No se Puede Empesar Sin Tu Apuesta Es Menor Al Credito",Toast.LENGTH_SHORT).show()
+        }
+    }
 }
+
