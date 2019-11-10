f1c2c655c978aa874426701ec2311909bcb9c134
Marpol96 <51094297+Marpol96@users.noreply.github.com>
2019-11-09 19:02:23 -0600

    avance3
    avance3

--- Project/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Project/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.text.Editable
+import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
+    val mRandom=Random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Button_Play.setOnClickListener{ChekcApuesta()}
+        Button_Play.setOnClickListener{
+            ChekcApuesta()
+            jugarya()
+        }
     }
 
     fun ChekcApuesta(){
@@ -22,5 +28,44 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Suerte!",Toast.LENGTH_SHORT).show()
         }
     }
-}
+    fun moverimagenes(): Int {
+        Vista1.setImageResource(R.drawable.mover)
+        Vista2.setImageResource(R.drawable.mover)
+        Vista3.setImageResource(R.drawable.mover)
+
+
+        return mRandom.nextInt(3 -1)+1
+
+    }
+
+    fun jugarya(){
+        var i=0
+        val slot= arrayListOf("avion","camion","tren")
+        while (i<=50000000) {
+            val s1 = moverimagenes()
+            val s2 = moverimagenes()
+            val s3 = moverimagenes()
+
+            if (s1==1){
+                slotimagen1()
+            }
+            i=i+1
+        }
+
+
+    }
+
+    fun slotimagen1(){
+        Vista1.setImageResource(R.drawable.avion)
+    }
+
+    fun slotimagen2(){
+        Vista2.setImageResource(R.drawable.camion)
+    }
+
+    fun slotimagen3(){
+        Vista3.setImageResource((R.drawable.tren))
+    }
+
+    }
 
